91a4dd31c868fe0b3dbfab9a6b61566e9aef60f3
rachel santamaria <rachel.santamaria@ucr.ac.cr>
2026-01-18 14:40:15 -0600

    Area Total

--- solid/openclose/fixed/solid/openclose/fixed/Main.java
+++ solid/openclose/fixed/solid/openclose/fixed/Main.java
@@ -2,12 +2,21 @@ package solid.openclose.fixed;
 
 public class Main {
     public static void main(String[] args) {
+        double totalArea = 0;
+
         Circle circle = new Circle(8.0);
         Rectangle rectangle = new Rectangle(6, 4);
         Triangle triangle = new Triangle(7, 5);
 
         System.out.println("Area of the circle: " + circle.calculateArea());
+        totalArea += circle.calculateArea();
         System.out.println("Area of the Rectangle: " + rectangle.calculateArea());
+        totalArea += rectangle.calculateArea();
         System.out.println("Area of the triangle: " + triangle.calculateArea());
+        totalArea += triangle.calculateArea();
+
+        System.out.println("Total area: " + totalArea);
+
+
     }
 }
